92005fd2ca20ae5c783d7be16c8eee5312f51ae3
ankushdogradev <ankushdogradev@gmail.com>
2021-09-14 19:17:59 +0530

    Project Created

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// > THEME SCSS: https://www.toptal.com/sass/theming-scss-tutorial
+//               https://medium.com/@dmitriy.borodiy/easy-color-theming-with-scss-bc38fd5734d1
+
+// > RESPONSIVE: https://www.youtube.com/watch?v=VQraviuwbzU
+//               https://raddy.co.uk/blog/advanced-media-queries-using-scss-part-1-2-and-3/
+
+// > DRAG & DROP: https://www.youtube.com/watch?v=4bzJrEETW4w
+//                https://www.youtube.com/watch?v=Q1PYQPK9TaM
+
+// > LIVE SEARCH: https://www.youtube.com/watch?v=4rPUo2urPJg
